fe5b685b38b70a7f9ae52d9fac0abd9f1502ab33
DaniloIDS <danilo.ids@outlook.com>
2021-11-28 15:14:29 -0300

    Function Carousel

---
+++ js/components/main-carousel.js
@@ -0,0 +1,20 @@
+// 1 Acessar o browser
+// 2 Acessar o documento (HTML)
+// 3 Pegar o botão
+// 4 Saber se o botão está sendo clicado
+
+const ButtonRight = window.document.querySelector('.button-arrow.-right');
+const ButtonLeft = window.document.querySelector('.button-arrow.-left');
+const Elements = window.document.querySelector('.elements');
+
+let pixels = 0;
+
+ButtonRight.addEventListener('click', function() {
+ pixels = pixels + 50;
+ Elements.style = `transform: translateX(${pixels}px);`;
+});
+
+ButtonLeft.addEventListener('click', function() {
+  pixels = pixels - 50;
+  Elements.style = `transform: translateX(${pixels}px);`;
+});
